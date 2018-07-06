59ac82b8ba2a5e746f449012642bb34684153890
abdulrozakgzn <abdulrozakcrb@gmail.com>
2018-07-06 23:53:25 +0700

    initial commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+
+<title>Cirebon Property Temukan Property Idaman Anda, Hunian / Villa / Pabrik / Gudang  | Home :: Cirebon Property</title>
+<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<style>
+body {
+
+	  margin: 0;
+	  padding: 0;
+	  width: 100%;
+	  height: 100%;
+	  color: #B0BEC5;
+	  display: table;
+	  font-weight: 100;
+	  font-family: 'Lato';
+}
+.container {
+			text-align: center;
+			display: table-cell;
+			vertical-align: middle;
+	}
+.content {
+		   text-align: center;
+		   display: inline-block;
+		 }
+.title  {
+			  font-size: 96px;margin-bottom: 40px;
+		}
+.quote {
+		font-size: 24px;
+	  }
+</style>
+
+</head>
+<body>
+	@include('shared.navbar');
+	
+</body>
+</html>
+
